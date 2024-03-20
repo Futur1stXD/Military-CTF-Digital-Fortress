ed657bf30d59b265511e5a5ed667bc57b8d2b9e6
futur1st <futur1stbtw@gmail.com>
2024-03-20 21:07:05 +0500

    fix

--- frontend/server.js
+++ frontend/server.js
@@ -18,6 +18,13 @@ app.get('*', (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
