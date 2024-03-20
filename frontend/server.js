6f00eb96a9fc9adc5e8a5667b32d2a4a4a9d7848
futur1st <futur1stbtw@gmail.com>
2024-03-20 17:58:01 +0500

    fix

--- frontend/server.js
+++ frontend/server.js
@@ -8,12 +8,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
-app.use(express.static('frontend/dist'));
-app.use('/src/img', express.static('frontend/src/img'));
+// Убедитесь, что пути указаны относительно текущей директории (__dirname)
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/src/img', express.static(path.join(__dirname, 'src', 'img')));
 
-app.get('*', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// Отправлять index.html только для корневого URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => console.log('Server has been started on port 3000...'));
