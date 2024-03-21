5c481da56137a9267ea639ba2c7770c222ce0af1
futur1st <futur1stbtw@gmail.com>
2024-03-21 13:05:36 +0500

    fix

--- frontend/src/pages/user/Profile.jsx
+++ frontend/src/pages/user/Profile.jsx
@@ -130,7 +130,7 @@ const Profile = ({ authenticated, token, isPhone={isPhone}}) => {
                                 
                                 <div className="max-w-3xl sm:px-6 flex flex-wrap ">
                                     {isPhone ? 
-                                    <div className="w-full md:w-5/6 p-4 mb-4 ml-8 md:ml-0 md:mb-0">
+                                    <div className="w-full md:w-4/6 p-4 mb-4 ml-8 md:ml-0 md:mb-0">
                                     
                                         <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg">
                                             <h3 className="text-lg font-bold text-gray-900 dark:text-white mb-2">Profile Tab</h3>
